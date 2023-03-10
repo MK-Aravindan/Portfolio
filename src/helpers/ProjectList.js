9a4153bc6bc02bff2521446a00a56083e24f8bd8
MK-Aravindan <aravindanmk02@gmail.com>
2023-03-10 20:48:59 +0530

    Add new Project

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,7 +2,7 @@ import Proj1 from "../assets/proj1.jpg";
 import Proj2 from "../assets/proj2.png";
 import Proj3 from "../assets/proj3.png";
 import Proj4 from "../assets/proj4.png";
-// import Proj5 from "../assets/proj5.jpg";
+import Proj5 from "../assets/proj5.avif";
 // import Proj6 from "../assets/proj6.png";
 
 export const ProjectList = [
@@ -10,31 +10,32 @@ export const ProjectList = [
     name: "sckascripts Blogger",
     image: Proj1,
     skills: "React JS, Spring Boot, MySQL.",
-    url: "https://github.com/MK-Aravindan/sckascripts-blogger.git"
+    url: "https://github.com/MK-Aravindan/sckascripts-blogger.git",
   },
   {
     name: "Jokes Generator",
     image: Proj2,
     skills: "React JS, Online API.",
-    url: "https://github.com/MK-Aravindan/jokes_generator.git"
+    url: "https://github.com/MK-Aravindan/jokes_generator.git",
   },
   {
     name: "Image Post Receiver",
     image: Proj3,
     skills: "React JS, Spring Boot, MySQL. Using LOB and BLOB method.",
-    url: "https://github.com/MK-Aravindan/image-PostReceiver.git"
+    url: "https://github.com/MK-Aravindan/image-PostReceiver.git",
   },
   {
     name: "Portfolio",
     image: Proj4,
     skills: "React JS",
-    url: "https://github.com/MK-Aravindan/Portfolio.git"
+    url: "https://github.com/MK-Aravindan/Portfolio.git",
   },
-  // {
-  //   name: "Dashboard Visualizer",
-  //   image: Proj5,
-  //   skills: "JavaScript,HTML,CSS",
-  // },
+  {
+    name: "JWT Authentication & Authorization",
+    image: Proj5,
+    skills: "Spring Boot, Spring Security and JSON Web Token.",
+    url: "https://github.com/MK-Aravindan/JWT-Authentication-Authorization.git",
+  },
   // {
   //   name: "Mobile Game",
   //   image: Proj6,
