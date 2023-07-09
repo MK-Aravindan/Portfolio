2c14928877f52c86e22146a87c693d11e90852b3
MK-Aravindan <aravindanmk02@gmail.com>
2023-07-09 11:18:39 +0530

    added css

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,7 +6,22 @@ import "../styles/Project.css";
 const Projects = () => {
   return (
     <div className="projects">
-      <h1>Personal Projects</h1>
+      <h1
+        style={{
+          fontSize: "30px",
+          textAlign: "center",
+          color: "#3e497a",
+          width: "fit-content",
+          marginTop: "-10px",
+          padding: "15px",
+          backgroundColor: "white",
+          borderRadius: "10px",
+          boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.2)",
+          display: "inline-block",
+        }}
+      >
+        Personal Projects
+      </h1>{" "}
       <div className="projectList">
         {ProjectList.map((project, idx) => {
           return (
